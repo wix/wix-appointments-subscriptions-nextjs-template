583e1342a83fb8ac821432876a83358d4c48f76d
Sarunas Dubinskas <sarunasd@wix.com>
2023-03-28 16:43:35 +0300

    Fixes navigation to Pricing Plans

--- src/model/paid-plans/paid-plans-checkout.ts
+++ src/model/paid-plans/paid-plans-checkout.ts
@@ -3,13 +3,13 @@ import type { plans } from '@wix/pricing-plans';
 export const getCheckoutUrl = ({
   plan,
   checkoutData,
-  maxStartDate,
 }: {
   plan: plans.PublicPlan;
   checkoutData?: string;
   maxStartDate?: string;
 }) => {
-  return `./api/pricing-plans-checkout?planId=${plan._id}&checkoutData=${
-    checkoutData ?? ''
-  }&maxStartDate=${maxStartDate ?? ''}`;
+  const params = new URLSearchParams();
+  params.set('planId', plan._id!);
+  params.set('checkoutData', checkoutData ?? '');
+  return `./api/pricing-plans-checkout?${params.toString()}`;
 };
