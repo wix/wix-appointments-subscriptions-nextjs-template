a50c3c4f35062dabdb0f515b7c0ffb56d921800c
carmelc <carmelc@wix.com>
2023-06-08 18:15:38 +0300

    initial e2e - vercel

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,8 +22,9 @@ const config = defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
-  snapshotPathTemplate:
-    '{testDir}/__screenshots__{/projectName}/{testFilePath}/{arg}{ext}',
+  snapshotPathTemplate: `{testDir}/__screenshots__/${
+    process.env.REMOTE_PROVIDER ?? 'local'
+  }{/projectName}/{testFilePath}/{arg}{ext}`,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
